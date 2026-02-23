23dbaa2faa3226ae2a6ff5c825d4704dfdca2093
Dani Imdadur <dir131203@gmail.com>
2026-02-23 14:36:46 +0700

    first commit

---
+++ src/main/java/com/gentara/elurah/SpringElurahApp.java
@@ -0,0 +1,13 @@
+package com.gentara.elurah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringElurahApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringElurahApp.class, args);
+    }
+
+}
