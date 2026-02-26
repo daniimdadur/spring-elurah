84da390e4fc20f1d071c8b7d9aba27123f1bd86f
mzaenalmustopa <zaenalebelmetal@gmail.com>
2026-02-26 16:49:49 +0700

    feat: update service impl

---
+++ src/main/java/com/gentara/elurah/master/controller/RtControllerApi.java
@@ -0,0 +1,45 @@
+package com.gentara.elurah.master.controller;
+
+import com.gentara.elurah.base.BaseController;
+import com.gentara.elurah.base.Response;
+import com.gentara.elurah.master.model.request.RtReq;
+import com.gentara.elurah.master.model.response.RtRes;
+import com.gentara.elurah.master.service.region.RtService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/api/rt")
+public class RtControllerApi extends BaseController<RtRes> {
+
+    private final RtService rtService;
+
+    @GetMapping
+    public ResponseEntity<Response> get() {
+        return super.getResponse(rtService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Response> get(@PathVariable("id") @NotBlank String id) {
+        return super.getResponse(rtService.getById(id));
+    }
+
+    @GetMapping()
+    public ResponseEntity<Response> post(@RequestBody @Valid RtReq request){
+        return super.getResponse(rtService.create(request));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Response> patch(@PathVariable("id") @NotBlank String id, @RequestBody @Valid RtReq request){
+        return super.getResponse(rtService.update(request, id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Response> delete(@PathVariable("id") @NotBlank String id) {
+        return super.getResponse(rtService.delete(id));
+    }
+}
